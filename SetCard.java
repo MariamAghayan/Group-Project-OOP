4c2d5334223612b4a39cf484c12931aa9683943b
MariamAghayan <101179912+MariamAghayan@users.noreply.github.com>
2022-05-10 01:28:24 +0400

    Update SetCard.java

--- SetCard.java
+++ SetCard.java
@@ -1,19 +1,19 @@
 package set.ui;
 
-import set.core.Board;
+import set.core.Card;
 
 import javax.swing.*;
 
 public class SetCard extends JButton {
-    private Board board = new Board();
     private int x;
     private int y;
     public SetCard(int x) throws CloneNotSupportedException {
         this.x = x;
     }
-    public void displayCards(String piece){
-        for (int i = 0; i < board.array.length; i++){
-            this.setIcon(new ImageIcon(board.array[i].toString() + ".gif"));
-        }
+
+    public void displayCards(Card piece) throws CloneNotSupportedException {
+            this.setIcon(new ImageIcon( "images\\" + piece.toString() + ".gif"));
+
     }
+
 }
