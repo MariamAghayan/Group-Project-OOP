a9bd6f38521a1690288a3f0e3e023923f579ae94
MariamAghayan <101179912+MariamAghayan@users.noreply.github.com>
2022-05-10 01:22:33 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
+package set;
+import set.ui.SetUI;
 
 public class Main {
-    public static void main(String[] args){
-        Card thh = new Card();
-        thh.array();
-        thh.copy();
-        thh.twelve();
-        thh.allSets();
-        thh.addThree();
+    public static void main(String[] args) throws CloneNotSupportedException {
+        SetUI game = new SetUI();
     }
 }
