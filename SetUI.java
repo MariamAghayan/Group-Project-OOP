76b98bd8f19a6597a1241cd9ea86063dcebbc1cf
MariamAghayan <101179912+MariamAghayan@users.noreply.github.com>
2022-05-10 01:59:50 +0400

    Update SetUI.java

--- SetUI.java
+++ SetUI.java
@@ -6,24 +6,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SetUI extends JFrame {
-    SetCard[] playthings = new SetCard[12];
+    SetCard[] playthings = new SetCard[15];
     SetCard button;
     JPanel panel;
     private Board board = new Board();
 
     public SetUI() throws CloneNotSupportedException {
         JFrame setWindow = new JFrame();
-        setWindow.setSize(650, 650);
+        setWindow.setSize(300, 300);
         setWindow.setTitle("Set Game");
         setWindow.setLocationRelativeTo( null );
         setWindow.setLayout(new FlowLayout(FlowLayout.CENTER));
         setWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel = new JPanel();
-        Dimension dim = new Dimension(600, 600);
+        Dimension dim = new Dimension(250, 250);
         panel.setMinimumSize(dim);
         panel.setPreferredSize(dim);
         panel.setSize(dim);
-        panel.setLayout(new GridLayout(3, 4));
+        panel.setLayout(new GridLayout(3, 4, 4, 4));
         setWindow.add(panel);
         fillTheBoard();
         setWindow.setVisible(true);
@@ -34,9 +34,8 @@ public class SetUI extends JFrame {
             panel.add(button);
             playthings[i] = button;
         }
+        for (int i = 0; i < 12; i++) playthings[i].displayCards(board.arrayNew.get(i));
 
-        for (int i = 0; i < 12; i++) playthings[i].displayCards(board.array2[i]);
     }
-
-
+    
 }
